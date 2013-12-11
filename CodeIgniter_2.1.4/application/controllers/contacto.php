3b275515c6c241284cf8c44fc27d1127b7e24491
diogoalfa <Diogo@MacBook-Pro-de-Diego.local>
2013-12-11 13:44:10 -0300

    Actualizar por que no pueden visualizar bootstrap

--- CodeIgniter_2.1.4/application/controllers/contacto.php
+++ CodeIgniter_2.1.4/application/controllers/contacto.php
@@ -15,8 +15,11 @@ class Contacto extends CI_Controller {
 		$this->load->view('general/headers');
                 $this->load->view('general/menu_principal');
                 $this->load->view('general/abre_bodypagina');
+                
                 $this->load->view('contacto/contacto_informacion');
                 $this->load->view('contacto/contacto_mail');
+                
+                
                 $this->load->view('general/cierre_bodypagina');
                 $this->load->view('general/cierre_footer');
 	}
