12bd2a5b38ae0130dae0f69de6d631ae56827c3f
diogoalfa <diogo.again@icloud.com>
2013-12-28 21:57:37 -0300

    Termine la seccion mostrar los estados de peticion del profesor-de pedidos

--- CodeIgniter_2.1.4/application/models/docente_model.php
+++ CodeIgniter_2.1.4/application/models/docente_model.php
@@ -33,19 +33,7 @@
     }
     public function academicoSemana($pk){
  
-       // $query=$this->db->query("SELECT s.sala, d.nombres,d.apellidos, a.nombre,c.seccion FROM reservas r, cursos c, docentes d, salas s, asignaturas a WHERE r.curso_fk=c.pk AND c.docente_fk=d.pk AND r.sala_fk=s.pk AND c.asignatura_fk=a.pk AND ".$pk."=d.pk");              
-       //  return $query->result();
-            $cond=array('d.pk'=>$pk);
-            $query=$this->db
-                ->select('p.periodo,p.inicio,p.termino, d.nombres,d.apellidos, a.nombre,s.sala,c.seccion')
-                ->from('reservas as r')
-                ->join('cursos as c','r.curso_fk=c.pk','inner')
-                ->join('docentes as d','c.docente_fk=d.pk','inner')
-                ->join('salas as s','r.sala_fk=s.pk','inner')
-                ->join('asignaturas as a','c.asignatura_fk=a.pk','inner')
-                ->join('periodos as p','p.pk=r.periodo_fk','inner')
-                ->where($cond)
-                ->get();
+       $query=$this->db->query("SELECT s.sala, d.nombres,d.apellidos, a.nombre,c.seccion FROM reservas r, cursos c, docentes d, salas s, asignaturas a WHERE r.curso_fk=c.pk AND c.docente_fk=d.pk AND r.sala_fk=s.pk AND c.asignatura_fk=a.pk AND ".$pk."=d.pk");              
         return $query->result();
        }
 
@@ -53,10 +41,10 @@
         $query=$this->db->query("SELECT * FROM docentes WHERE rut='$rut'");
          return $query->row();
     }     
-     function getAsignatura($pk) {
+     function getAsignatura($pk_docente) {
         $query=$this->db->query("SELECT a.nombre as nombre, a.pk as pk 
                                  FROM cursos as c,asignaturas as a 
-                                 WHERE  c.docente_fk='$pk' AND a.pk=c.asignatura_fk;");
+                                 WHERE  c.docente_fk='$pk_docente' AND a.pk=c.asignatura_fk;");
         return $query->result();
      }
      
@@ -67,5 +55,16 @@
                        . "(SELECT pk FROM cursos WHERE asignatura_fk='$asignatura_pk' and docente_fk='$docente_pk' ));");
          return true;
      }
+     
+     public function getPedidoSalaDocente($asignatura_pk,$docente_pk){
+//$query=$this->db->query("SELECT * FROM reservas WHERE curso_fk=(SELECT pk FROM cursos WHERE asignatura_fk='$asignatura_pk' AND docente_fk='$docente_pk') ");
+         
+      $query=$this->db->query("SELECT r.*,s.sala,p.periodo FROM reservas as r,salas as s,periodos as p WHERE r.curso_fk=(SELECT pk FROM cursos WHERE asignatura_fk='$asignatura_pk' AND docente_fk='$docente_pk') "
+              . "AND s.pk=r.sala_fk AND p.pk=r.periodo_fk ");
+         
+      return $query->result();
+     }
+     
+     
    }
 ?>
